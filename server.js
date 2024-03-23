e56dcc80f394c63aeefe2b8a9d7e0cbd36bfe061
Tamerbekir <astoryofame@gmail.com>
2024-03-23 18:01:52 -0400

    changed boiler plate in server.js. Changed sessions to sess in .env file.

--- server.js
+++ server.js
@@ -8,26 +8,31 @@ const SequelizeStore = require('connect-session-sequelize')(sessions.Store)
 const session = require('express-session')
 
 const app = express()
-const  PORT = process.env.PORT ||  3001;
+const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}))
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.use(routes)
 
-sequelize.sync({ force: false }).then(()=>
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`)))
+sequelize.sync({ force: false }).then(() =>
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`)))
